c2dca6ccc22b25dd13f0179eb60c27b6776a538a
Thomas <wall.thomas.a22@student.dhbw-karlsruhe.de>
2025-02-02 16:27:09 +0100

    Fix

--- app/src/main/java/de/bilkewall/main/di/DrinkListViewModelFactory.kt
+++ app/src/main/java/de/bilkewall/main/di/DrinkListViewModelFactory.kt
@@ -1,14 +1,9 @@
-// DrinkListViewModelFactory.kt
 package de.bilkewall.main.di
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import de.bilkewall.application.repository.DrinkRepositoryInterface
 import de.bilkewall.adapters.viewmodel.DrinkListViewModel
 import de.bilkewall.application.service.database.DrinkService
-import de.bilkewall.main.di.DependencyProvider.drinkRepository
-import de.bilkewall.main.di.DependencyProvider.getAllDrinksService
-import de.bilkewall.main.di.DependencyProvider.getDrinksByNameService
 
 class DrinkListViewModelFactory(
     private val drinkService: DrinkService
